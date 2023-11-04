6e20d8f13cc474ef9addc514a2bd5ffe62dddb53
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-04 19:32:30 +0000

    Add files via upload
    Basic use of integers and floats

---
+++ Variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    
+    // Separate declaration and initialisation
+    int number;
+    number = 128;
+    printf("%d\n", number);
+    
+    // Combined declaration and initialisation
+    float pi = 3.14159;
+    printf("Pi is roughly equivalent to %f\n", pi);
+    
+    return 0;
+}
