efaa53b9958ef8a7051e0d275159225ff772e8b6
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-07 16:24:10 +0000

    Update Basic Game.py
    Fixed issue where players lost lives on their turn from overflowing damage

--- Basic Game.py
+++ Basic Game.py
@@ -22,6 +22,7 @@ while gameRunning:
     if player2.health <= 0:
         print("Player 2 has lost a life!")
         player2.lives -= 1
+        player2.health = 100
         
         # Player 2 loses if out of lives and killed. Game ends
         if player2.lives <= 0: 
@@ -29,21 +30,19 @@ while gameRunning:
             gameRunning = False
         else:
             print("Player 2 has " + str(player2.lives) + " lives remaining")
-            player2.health = 100
             
-    else:
-        # Player 2 attacks Player 1
-        player2Attack = int(input("Enter a number to deal damage: "))
-        player1.health -= player2Attack
-        print("Player 1 has ", str(player1.health) + " health remaining.")
+    # Player 2 attacks Player 1
+    player2Attack = int(input("Enter a number to deal damage: "))
+    player1.health -= player2Attack
+    print("Player 1 has ", str(player1.health) + " health remaining.")
     
     # Player 1 loses a life if health reaches 0 or below
     if player1.health <= 0:
         player1.lives -= 1
+        player1.health = 100
         # Player 1 loses if out of lives and killed. Game ends
         if player1.lives <=0:
             print("Player 1 is out of lives. Player 2 wins!")
             gameRunning = False
         else:
             print("Player 1 has " + str(player1.lives) + " lives remaining")
-    
