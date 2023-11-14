04e833cbeabc4af2120c1faa95341fc2d0a2218d
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-14 16:03:32 +0000

    Add files via upload

---
+++ Conditionals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int a = 2;
+int b = 3;
+
+if (a > b) {
+    printf("%i\n", a);
+} else if (b > a) {
+    printf("%i\n", b);
+} else {
+    printf("%s\n", "a is equal to b");
+}
