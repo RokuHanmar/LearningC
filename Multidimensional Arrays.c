76a635fa2a4c9705e49aac25632fb923a3035a31
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-18 20:02:58 +0000

    Add files via upload

---
+++ Multidimensional Arrays.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+int cards[2][13] = {
+    {1, 2, 3, 4},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}};
+    printf("%d\n", cards[0][0]);
+
+    return 0;
+}
