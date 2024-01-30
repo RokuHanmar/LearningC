d25b25f6a8d4c6b3ba1c35e2d49ebebb3620ad58
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-01-30 10:19:41 +0000

    Add files via upload

---
+++ Strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char text[] = "Lorem Ipsum";
+    printf("%s\n", text);
+
+    char conversation[] = "The song is called \"Fear of the Dark\" and it's by Iron Maiden";
+    printf("%s\n", conversation);
+
+    return 0;
+}
